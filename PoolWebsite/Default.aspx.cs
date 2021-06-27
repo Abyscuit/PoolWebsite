cc39420312687c41240a4094c2ec29c702585c1d
abyscuit <marcus14530@gmail.com>
2021-06-27 03:12:09 -0700

    Fixed balance display

--- PoolWebsite/Default.aspx.cs
+++ PoolWebsite/Default.aspx.cs
@@ -17,6 +17,7 @@ namespace PoolWebsite
         public static decimal bal = 0;
         protected void Page_Load(object sender, EventArgs e)
         {
+            
             var balance = web3.Eth.GetBalance.SendRequestAsync("0x41A50eF9579aA9E21C48DCC707BB14e30C9c60fA");
             bal = Web3.Convert.FromWei(balance.Result.Value);
         }
